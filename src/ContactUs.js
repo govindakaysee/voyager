b4a7d03f7391d54142d98178942771fa01de6300
govindakaysee <63325747+govindakaysee@users.noreply.github.com>
2021-06-06 14:15:18 -0500

    Changes on lake description

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -1,3 +1,14 @@
 export function ContactUs(){
-    return <p>This is Contact us page</p>;
+    return (
+    <div className = "about-us">
+        <h1>About US</h1>
+        <h2> Who are we? </h2>
+        <p>
+            We are a non-profit organization whose sole motive is to help the local and non-local people who want to visit the places of dallas to get full and correct information within a same platform.
+            This website suggest the best lakes in dallas area and also the facilities around those lakes with best deals. We were established on 2021 and have been working on to improve the website to provide our users with correct and updated information.
+            We have a group of young, creative and hardworking group of programmers who are devoted to work 24hrs to provide the best services to the user. 
+            
+        </p>
+    </div>
+    );
 }
---
+++ src/data/lakes.js
@@ -0,0 +1,89 @@
+export const lakes = [
+    {
+        id: "lake_ray",
+        name: "Lake Ray Hubbard",
+        location: "Northeast Dallas",
+        description: "It is also known as lake Forney which covers 9205 hectares of land. It is one of the largest manmade lakes in north Texas which sits on the East fork of the Trinity River. The most popular attractions 21 are six parks, three marinas, and four boat ramps. Boat, kayak, canoe, and paddleboard rentals are available, and visitors can book the fishing spot.",
+        website: "",
+        profileImage: "/images/arlingtonlake.jpeg"
+    },
+    {
+        id: "white_rock",
+        name: " White rock lake",
+        location: " East Dallas",
+        description: "It covers 507-hectare land, and it is popular for kayaking, canoeing, and paddleboarding which can be booked right at the lake. People go hiking, cycling weekly, and the Dallas Arboretum and Botanical Garden sit on the lake’s shoreline.", 
+        website: "",
+        profileImage: ""
+
+    },
+    {
+        id: "grapevine_lake",
+        name: "Grapevine Lake",
+        location: "Grapevine",
+        description: "It is the most popular lake in the DFW area which covers 2950 hectares of land. It offers different recreational activities such as boating, fishing, kayaking, jet skiing and water skiing for locals of the area and visitors. This lake is best for campgrounds and cabin rentals.", 
+        website: "",
+        profileImage: ""
+    },
+    {
+        id: "bachman_lake",
+        name: "Bachman Lake",
+        location: "Northwest Dallas",
+        description: "It occupies a small area of 205 acres and is located in the northwest boundary of Dallas Love Field airport. It is popular among local people and visitors. It includes a five thousands hiking and bike trail, boating, recreation center and an indoor aquatic center which makes it safer and more popular.", 
+        website: "",
+        profileImage: ""
+    },
+
+    {
+        id: "joe_pool",
+        name: "Joe Pool Lake",
+        location: "Cedar Hill",
+        description: "It is named after Congressman Joe Poll, who funded the building of the lake. It covers 3130 hectares of land where six parks are located along the lake’s shoreline. The lake is famous for hiking trails, picnic areas, swimming beaches, camping sites, and boat ramps.",  
+        website: "",
+        profileImage: ""
+    },
+
+    {
+        id: "mountain_creek",
+        name: "Mountain Creek lake",
+        location: "Grand Praire",
+        description: "It was originally built as a cooling reservoir for an electric power plant but today it is used for recreational purposes. Some of the promising features of this lake are a baseball field, picnic tables, fishing, boat ramp, playground, and soccer field. The south side of the park is the Prairie Lakes Golf Course.",  
+        website: "",
+        profileImage: ""
+    },
+
+    {
+        id: "lake_tawankoni",
+        name: "Lake Tawakoni",
+        location: "West Tawankoni",
+        description: "It covers 15329 hectares of land east of Dallas. The lake was named after native American people as they inhabited this land where the lake now sits. It is used by people for water supply and recreation activities. Swimming, picnicking, camping, boating, and fishing are some of the fun parts of this lake.",  
+        website: "",
+        profileImage: ""
+    },
+
+    {
+        id: "cedar_creek",
+        name: "Cedar Creek Reservoir",
+        location: "Henderson and Kaufman Counties",
+        description: "The 2990 hectare lake stretches across two counties. It is the fourth largest lake in Texas which is famous for fishing. Visitors can find plenty of blue, flathead, and channel catfish, white bass, and crappie. The best time to go fishing is spring and summer break.",  
+        website: "",
+        profileImage: ""
+    },
+
+    {
+        id: "arlington_lake",
+        name: "Lake Arlington",
+        location: "Arlington",
+        description: "This is a go-to lake for UT Arlington students. This lake is close to the university so most of the visitors are students. It runs from Bowman Spring Park to Richard Simpson Park. Paddling, picnicking and fishing are popular here and moreover, this lake hosts the Arlington Bass Classic each year in April.",  
+        website: "",
+        profileImage: ""
+    },
+
+    {
+        id: "lewisville_lake",
+        name: "Lewisville Lake",
+        location: "Lewisville",
+        description: "Originally this lake was called Lake Dallas but later it was switched to Lewisville. It is used for flood control, water supply, and recreational activities. It covers 11975 hectares of land and gets busy during summer break. People can enjoy six marinas, three restaurants, 12 parks, hiking trails, a campground and boat ramps during the weekend.",  
+        website: "",
+        profileImage: ""
+    }
+];
