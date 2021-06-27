05c80843bc6fe071345352937687380e0be314aa
govindakaysee <63325747+govindakaysee@users.noreply.github.com>
2021-06-27 13:10:03 -0500

    login/logout

--- src/components/LoginLogoutBtn.js
+++ src/components/LoginLogoutBtn.js
@@ -1,17 +1,25 @@
 import { useContext } from 'react';
 import { Link } from 'wouter';
+import firebase from 'firebase/app';
+import "firebase/auth";
+
 import { AppContext } from '../AppContext';
 
 export function LoginLogoutBtn() {
     const { isLoggedIn } = useContext(AppContext);
+
+    const handleClick = () => {
+        if (isLoggedIn) {
+            console.log('Logout button clicked.');
+            firebase.auth().signOut();
+        }
+    };
+
     return (
         <Link
             href={isLoggedIn ? "/" : "/login"}
             className={`login-logout-btn ${isLoggedIn ? 'logout-btn' : 'login-btn'}`}
-            onClick={() => {
-                if (isLoggedIn) console.log('Logout button clicked.');
-                else console.log('Login button clicked.');
-            }}
+            onClick={handleClick}
         >
             {isLoggedIn ? "Logout" : "Login" }
         </Link>
--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,21 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useContext } from 'react';
+import { Link } from 'wouter';
+import firebase from 'firebase/app';
+
 import { AppContext } from '../../AppContext';
 
+import "./Login.css";
+import useLocation from 'wouter/use-location';
+
 export function Login() {
+    const [, setLocation] = useLocation();
     const { isLoggedIn } = useContext(AppContext);
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
+    const [ error, setError ] = useState("");
+
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(() => {
+                setError("");
+                setLocation("/");
+            })
+            .catch((error) => {
+                console.log(error);
+                setError(error.message);
+            });
+    };
+
     if (!isLoggedIn){
         return (
-            <div className= "login-page">
-                <div className= "form">
-                    <form className= "register-form">
-                        <input type="text" placeholder="Name"/>
-                        <input type="text" placeholder="Password"/>
-                        <input type="text" placeholder="Email id"/>
-                        <input type="number" placeholder="Phone Number"/>
-                        <input type="date"/>
-                        <button>Create</button>
+            <div className= "auth-page">
+                <div className="auth-container">
+                    <form className= "login-form" onSubmit={handleOnSubmit}>
+                        <h3>Login Form</h3>
+                        <input
+                            type="email"
+                            onChange={({ target }) => setEmail(target.value)}
+                            placeholder="example@example.com"
+                        />
+                        <input
+                            type="password"
+                            onChange={({ target }) => setPassword(target.value)}
+                            placeholder="Enter password"
+                        />
+                        <input type="submit" value="Log In" />
+                        {
+                            error
+                                ? <small className="auth-error">{error}</small>
+                                : null
+                        }
                     </form>
+                    <Link href="/register">
+                        <small className="no-account-text">Don't have an account? Create one. -&gt;</small>
+                    </Link>
                 </div>
             </div>           
         );
